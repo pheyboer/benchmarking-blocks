b51533fea3e4c3e1a9b7091db7ab4ee731044974
Peter Heyboer <heyboer.peter@gmail.com>
2025-03-06 14:11:14 -0500

    checking with varying long strings

--- benchmark_with_block.rb
+++ benchmark_with_block.rb
@@ -8,7 +8,11 @@ end
  
 # Be careful, pasting this into IRB will take a long time to print.
 # It's a loooong string. :)
-long_string = "apple"*100000000
+# long_string = "apple"*100000000
+
+# long_string = "apple"*100_000
+
+long_string = "apple"*1_000_000
  
 running_time = benchmark { long_string.reverse }
  
